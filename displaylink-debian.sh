57fac7b4fd6a879979bb7fe50901a6a49e30cc71
Adnan Hodzic <adnan-hodzic@idexx.com>
2015-11-29 11:25:11 +0100

    Added post uninstall file cleanup

--- displaylink-debian.sh
+++ displaylink-debian.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #
-# DisplayLink driver installer for Debian (Stretch)
+# DisplayLink driver installer for Debian GNU/Linux
 #
 # Copyleft: Adnan Hodzic <adnan@hodzic.org>
 # License: GPLv3
 
 driver_dir=1.0.138
-mkdir -p $driver_dir
+
+# ToDo: add dependency check on:
+# unzip linux-headers-$(uname -r) dkms lsb-release
 
 distro_check(){
 
@@ -62,14 +64,12 @@ echo -e "\nDownloading DisplayLink Ubuntu driver:"
 wget http://downloads.displaylink.com/publicsoftware/DisplayLink-Ubuntu-1.0.138.zip
 
 # prep
+mkdir $driver_dir
 echo -e "\nPrepring for install ...\n"
 unzip -d $driver_dir DisplayLink-Ubuntu-1.0.138.zip
 chmod +x $driver_dir/displaylink-driver-1.0.138.run
 ./$driver_dir/displaylink-driver-1.0.138.run --keep --noexec
-
-# prep2
 mv displaylink-driver-1.0.138/ $driver_dir/displaylink-driver-1.0.138
-#cp -f displaylink-installer.sh $driver_dir/displaylink-driver-1.0.138
 
 # modify displaylink-installer.sh
 sed -i "s/SYSTEMINITDAEMON=unknown/SYSTEMINITDAEMON=systemd/g" $driver_dir/displaylink-driver-1.0.138/displaylink-installer.sh
@@ -95,19 +95,21 @@ echo -e "\nUninstalling ...\n"
 cd $driver_dir/displaylink-driver-1.0.138 && sudo ./displaylink-installer.sh uninstall
 sudo rmmod evdi
 
-# clean-up
-# ToDo: 
-# find and display dir/file for cleanup
-# after confirmation delete them
-#
-#echo -e "Performing clean-up"
-#rm -r $driver_dir
-#rm DisplayLink-Ubuntu-1.0.138.zip
+# cleanup
+# Todo: add confirmation before removing
+cd -
+rm -r $driver_dir
+rm DisplayLink-Ubuntu-1.0.138.zip
 
 echo -e "\nUninstall complete\n"
 }
 
-echo -e "\nDisplayLink driver for Debian Stretch (kernel 4.2)\n"
+post(){
+eval $(rm -r $driver_dir)
+eval $(rm DisplayLink-Ubuntu-1.0.138.zip)
+}
+
+echo -e "\nDisplayLink driver for Debian GNU/Linux\n"
 
 read -p "[I]nstall
 [U]ninstall
@@ -121,8 +123,8 @@ then
 elif [[ $answer == [Uu] ]];
 then
 	uninstall
+	#post
 else
 	echo -e "\nWrong key, aborting ...\n"
 	exit 1
 fi
-
