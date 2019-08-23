b3f92cad3d5a0a083500e60d9d6c4dd457075b23
Quentin BUISSON-DEBON <quentin.buisson-debon@bottomline.com>
2019-08-23 18:00:36 +0200

    Improve OS detection

--- displaylink-debian.sh
+++ displaylink-debian.sh
@@ -118,6 +118,53 @@ do
 done
 }
 
+# # Detects which OS and if Linux will also detect which Linux Distribution (Better than the current code ?)
+# get_distribution_name () {
+#   OS=`uname -s`
+#   REV=`uname -r`
+#   MACH=`uname -m`
+
+#   get_distribution_version_from_file()
+#   {
+#     VERSION=`cat $1 | tr "\n" ' ' | sed s/.*VERSION.*=\ // `
+#   }
+
+#   if [ "${OS}" = "SunOS" ] ; then
+#     OS=Solaris
+#     ARCH=`uname -p`
+#     OSSTR="${OS} ${REV}(${ARCH} `uname -v`)"
+#   elif [ "${OS}" = "AIX" ] ; then
+#     OSSTR="${OS} `oslevel` (`oslevel -r`)"
+#   elif [ "${OS}" = "Linux" ] ; then
+#     KERNEL=`uname -r`
+#     if [ -f /etc/redhat-release ] ; then
+#       DIST='RedHat'
+#       PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
+#       REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
+#     elif [ -f /etc/SuSE-release ] ; then
+#       DIST=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
+#       REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
+#     elif [ -f /etc/mandrake-release ] ; then
+#       DIST='Mandrake'
+#       PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
+#       REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
+#     elif [ -f /etc/debian_version ] ; then
+#       DIST="Debian `cat /etc/debian_version`"
+#       REV=""
+
+#     fi
+#     if [ -f /etc/UnitedLinux-release ] ; then
+#       DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
+#     fi
+
+#     OSSTR="${OS} ${DIST} ${REV}(${PSUEDONAME} ${KERNEL} ${MACH})"
+
+#   fi
+
+#   #echo ${OSSTR}
+#   echo ${DIST}
+# }
+
 distro_check(){
 separator
 # RedHat
