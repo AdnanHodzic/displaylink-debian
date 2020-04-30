2235e9348c7cac1410da6efe7b7a1b3eaa547eca
Erwin D <31799121+Erwin-D@users.noreply.github.com>
2020-04-30 10:41:33 +0200

    Update evdi.sh
    The updated file provides a workaround for users experiencing patching errors with the evdi in master the branch.

--- evdi.sh
+++ evdi.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-apt-get install -y libdrm-dev libelf-dev
-git clone https://github.com/DisplayLink/evdi.git
+wget https://github.com/DisplayLink/evdi/archive/v1.6.4.tar.gz
+tar -zxvf v1.6.4.tar.gz 
+mv evdi-1.6.4/ evdi
 cd evdi
 wget https://crazy.dev.frugalware.org/evdi-all-in-one-fixes.patch
+sed -i -E 's/\@\@ -26,6 \+26,8 \@\@ env:/\@\@ -25,6 \+25,9 \@\@ env:\n   \- KVER\=4.20/g' evdi-all-in-one-fixes.patch
+sed -i -E 's/   \- KVER=5.2/\+  \- KVER=5.2/g' evdi-all-in-one-fixes.patch
 patchPath="$(pwd)/evdi-all-in-one-fixes.patch"
 patch -Np1<$patchPath
 sed -E -e 's:SUBDIRS=([^ ]+) :M=\1 &:g' -i 'module/Makefile'
