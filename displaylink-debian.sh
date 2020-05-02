9a79a21031d2d458e27ea5e6d8274eabd50db032
Adnan Hodzic <adnan@hodzic.org>
2020-05-02 21:00:41 +0200

    added "libdrm-dev" as dep (close #409)

--- displaylink-debian.sh
+++ displaylink-debian.sh
@@ -93,12 +93,12 @@ echo -e "\nChecking dependencies\n"
 
 if [ "$lsb" == "Deepin" ];
 then
-	deps=(unzip linux-headers-$(uname -r) dkms lsb-release linux-source-deepin x11-xserver-utils wget)
+	deps=(unzip linux-headers-$(uname -r) dkms lsb-release linux-source-deepin x11-xserver-utils wget libdrm-dev)
 elif [ "$lsb" == "PureOS" ];
 then
 	deps=(unzip linux-headers-$(uname -r) dkms lsb-release linux-source x11-xserver-utils wget libdrm-dev)
 else
-	deps=(unzip linux-headers-$(uname -r) dkms lsb-release linux-source x11-xserver-utils wget)
+	deps=(unzip linux-headers-$(uname -r) dkms lsb-release linux-source x11-xserver-utils wget libdrm-dev)
 fi
 
 for dep in ${deps[@]}
