34bd67446f9eb2979f3728f75932398d7cdfb7a1
Rob Hendricks <robstahendricks@gmail.com>
2020-05-05 11:32:20 +0100

    Patches attempted on fixed files... (#415)
    * Installation fails if a prior $driver_dir folder exists (cannot mkdir)
    Removing older instance so we can download latest...
    
    * fix issue where patch unnecessarily applied after its been fixed
    
    * fix issue where patch unnecessarily applied after its been fixed
    
    Co-authored-by: Adnan Hodzic <adnan@hodzic.org>

--- evdi.sh
+++ evdi.sh
@@ -1,13 +1,87 @@
 #!/bin/bash
-wget https://github.com/DisplayLink/evdi/archive/v1.6.4.tar.gz
-tar -zxvf v1.6.4.tar.gz 
-mv evdi-1.6.4/ evdi
-cd evdi
-wget https://crazy.dev.frugalware.org/evdi-all-in-one-fixes.patch
-sed -i -E 's/\@\@ -26,6 \+26,8 \@\@ env:/\@\@ -25,6 \+25,9 \@\@ env:\n   \- KVER\=4.20/g' evdi-all-in-one-fixes.patch
-sed -i -E 's/   \- KVER=5.2/\+  \- KVER=5.2/g' evdi-all-in-one-fixes.patch
-patchPath="$(pwd)/evdi-all-in-one-fixes.patch"
-patch -Np1<$patchPath
-sed -E -e 's:SUBDIRS=([^ ]+) :M=\1 &:g' -i 'module/Makefile'
+
+apt-get install -y libdrm-dev libelf-dev
+
+resourcesDir="$(pwd)/resources/"
+evdiDir="$(pwd)/evdi/"
+externalPatchDomain="https://crazy.dev.frugalware.org/"
+
+#EVDI patch local resources
+currentEvdiPatch="eaiof.patch"
+patchFileName="evdi-all-in-one-fixes.patch"
+
+
+#EVDI Web resources
+externalPatchURL=$externalPatchDomain$patchFileName
+evdiURL="https://github.com/DisplayLink/evdi.git"
+
+#Patch paths 
+currentPatchPath=$resourcesDir$currentEvdiPatch
+commitPatchPath=$resourcesDir$patchFileName
+finalPatchPath="$evdiDir$patchFileName"
+
+#Triggers - whether we need to patch or not
+performPatchCheck="false"
+commitPatch="false"
+
+read -p "STOP - Hammer Time"
+
+#Check existing local resources
+if [ ! -d $resourcesDir ] ; then
+	echo "Making $resourcesDir"
+	mkdir $resourcesDir
+elif [ ! -f $currentPatchPath ] ; then
+	echo "Will create new patch file"
+else
+	performPatchCheck="true"
+fi
+
+
+cd "$resourcesDir"
+#remove all files except current local patch - if it's present
+find . ! -name $currentEvdiPatch -delete
+
+#download any new patch
+echo "$externalPatchURL"
+
+read -p "STOP - Hammer Time"
+wget -N $externalPatchURL
+#Check wget was successful
+if [ $? -eq 0 ] ; then
+	updateCurrentPatch="true"
+
+	if [ "$performPatchCheck" = "true" ] ; then
+		difFiles="$(ls | tr '\n' ' ')"
+		if diff $difFiles &> /dev/null ; then
+			updateCurrentPatch="false"
+		fi
+	fi
+	
+	if [ "$updateCurrentPatch" = "true" ]; then 
+		cp $commitPatchPath $currentPatchPath
+		##### APPLY THE PATCH TO EVDI #####
+		commitPatch="true"
+	fi
+fi
+
+cd ..
+
+#Cleanup old EVDI folder
+if [ -d $evdiDir ] ; then
+	rm -rf $evdiDir
+fi
+
+read -p "STOP - Hammer Time"
+
+git clone $evdiURL
+cd "$evdiDir"
+if [ "$commitPatch" = "true" ] ; then
+	cp $commitPatchPath $evdiDir
+
+	read -p "STOP - Hammer Time"
+	patch -Np1<$finalPatchPath
+	sed -E -e 's:SUBDIRS=([^ ]+) :M=\1 &:g' -i 'module/Makefile'
+fi
+
 make
 cd ..
