f596022f06442719b5ff5e4297feab7cce94ec51
Dory-dev <31799121+Dory-dev@users.noreply.github.com>
2020-11-13 13:12:38 +0100

    Refactor evdi.sh All patches have been fixed upstream, so there is no need to use it here Delete eaiof.patch

--- evdi.sh
+++ evdi.sh
@@ -2,66 +2,9 @@
 
 resourcesDir="$(pwd)/resources/"
 evdiDir="$(pwd)/evdi/"
-externalPatchDomain="https://crazy.dev.frugalware.org/"
-
-#EVDI patch local resources
-currentEvdiPatch="eaiof.patch"
-patchFileName="evdi-all-in-one-fixes.patch"
-
-# displaylink-installer patch local resources
-displayLinkInstallerPatch="displaylink-installer.patch"
 
 #EVDI Web resources
-externalPatchURL=$externalPatchDomain$patchFileName
 evdiURL="https://github.com/DisplayLink/evdi.git"
-
-#Patch paths 
-currentPatchPath=$resourcesDir$currentEvdiPatch
-commitPatchPath=$resourcesDir$patchFileName
-finalPatchPath="$evdiDir$patchFileName"
-
-#Triggers - whether we need to patch or not
-performPatchCheck="false"
-commitPatch="false"
-
-#Check existing local resources
-if [ ! -d $resourcesDir ] ; then
-	echo "Making $resourcesDir"
-	mkdir $resourcesDir
-elif [ ! -f $currentPatchPath ] ; then
-	echo "Will create new patch file"
-else
-	performPatchCheck="true"
-fi
-
-
-cd "$resourcesDir"
-#remove all files except current local patch and display-link-installer.patch - if it's present
-find . ! -name $currentEvdiPatch $displayLinkInstallerPatch  -delete
-
-#download any new patch
-echo "$externalPatchURL"
-
-wget -N $externalPatchURL
-#Check wget was successful
-if [ $? -eq 0 ] ; then
-	updateCurrentPatch="true"
-
-	if [ "$performPatchCheck" = "true" ] ; then
-		difFiles="$(ls | tr '\n' ' ')"
-		if diff $difFiles &> /dev/null ; then
-			updateCurrentPatch="false"
-		fi
-	fi
-	
-	if [ "$updateCurrentPatch" = "true" ]; then 
-		cp $commitPatchPath $currentPatchPath
-		##### APPLY THE PATCH TO EVDI #####
-		commitPatch="true"
-	fi
-fi
-
-cd ..
 
 #Cleanup old EVDI folder
 if [ -d $evdiDir ] ; then
@@ -70,12 +13,5 @@ fi
 
 git clone --depth=1 --branch v1.7.0 $evdiURL
 cd "$evdiDir"
-if [ "$commitPatch" = "true" ] ; then
-	cp $commitPatchPath $evdiDir
-	
-	patch -Np1<$finalPatchPath
-	sed -E -e 's:SUBDIRS=([^ ]+) :M=\1 &:g' -i 'module/Makefile'
-fi
-
 make
 cd ..
