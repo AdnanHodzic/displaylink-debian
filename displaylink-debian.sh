4edc6ee02402db6cf2e375e7fd36c762c5eda18b
Barabas <31799121+Barabazs@users.noreply.github.com>
2022-05-29 08:38:52 +0200

    Update displaylink-debian.sh
    Remove obsolete comment

--- displaylink-debian.sh
+++ displaylink-debian.sh
@@ -16,7 +16,6 @@ set -eu
 # set -o pipefail # TODO: Some code still fails this check, fix before enabling.
 IFS=$'\n\t'
 
-# Latest regular release only support kernel version <= 5.13
 kernel_check="$(uname -r | egrep -o '[0-9]+\.[0-9]+')"
 max_kernel_version_supported="5.17"
 
